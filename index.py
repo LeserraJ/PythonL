07be5a553da710b8c74c76adbe5ccd99c02007e4
LeserraJ <leserrabusiness@gmail.com>
2024-01-30 05:07:50 +0000

    added basic idea, need to add nations selection and rename varibales to match nation

---
+++ index.py
@@ -0,0 +1,15 @@
+#input that takes in a string --> compares string to array --> gives list of what vehicles to expect
+
+
+Rank1 = 1
+Rank2 = 2
+Rank3 = 3
+Rank4 = 4
+Rank5 = 5
+Rank6 = 6
+Rank7 = 7
+
+
+Ranks = [Rank1,Rank2,Rank3,Rank4,Rank5,Rank6,Rank7]
+
+print(Ranks)
