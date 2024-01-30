f6f559d5da1e5c63f17917f75cb9d9b9fe335591
LeserraJ <leserrabusiness@gmail.com>
2024-01-30 21:18:37 +0000

    new idea

---
+++ br1.py
@@ -0,0 +1,6 @@
+
+tankSet = {"M2A4" , "M2", "T34"}
+
+for tanks in tankSet:
+    print(tanks)
+
--- index.py
+++ index.py
@@ -1,24 +1,20 @@
 #input that takes in a string --> compares string to array --> gives list of what vehicles to expect
-import json
-Rank1_USAG = {"M2A4 BR: Reserve", "M2 BR: 1.3" , "LVT(A)(1) BR: Reserve", "M13 MGMC BR: 1.7"}
-USA = frozenset({Rank1_USAG})
-Nations = [USA]
+#import json
+#Rank1_USAG = {"M2A4 BR: Reserve", "M2 BR: 1.3" , "LVT(A)(1) BR: Reserve", "M13 MGMC BR: 1.7"}
+#USA = frozenset({Rank1_USAG})
+#Nations = [USA]
 #USA_json = json.dumps(USA)
 #What tank are you in --> user input --> outputs what other tanks are in that br range of (1.0 Lower -- Your BR -- 1.0 Higher)
-User_Tank = str(input('What tank are you in ?\n'))
-Tank_Found = False
-for User_Tank in Nations:
-    if User_Tank == theTank:
-        Tank_Found = True
-        break
-    print('Ok scanning for other tanks.')
+#User_Tank = str(input('What tank are you in ?\n'))
+#Tank_Found = False
+#for User_Tank in Nations:
+    #if User_Tank == theTank:
+        #Tank_Found = True
+        #break
+    #print('Ok scanning for other tanks.')
 
-    if not Tank_Found:
-      raise ValueError('Please enter a valid tank. \n')   
-    
-   
-      
-    
+    #if not Tank_Found:
+      #raise ValueError('Please enter a valid tank. \n')   
 
 
 
