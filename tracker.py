7f8569fc05b3a173604f8aa4d12d9d8f3570ad09
LeserraJ <leserrabusiness@gmail.com>
2024-02-01 18:24:54 +0000

    added tracker for legs

---
+++ tracker.py
@@ -0,0 +1,8 @@
+#LEGS
+# Walk 5-10 Min, then dynamic stretches
+# Heavy Squat: (85-90% of 1rpm) 1x2-4 --> Paused Squat: (75% of Heavy Squat) 2x5
+# RDL 3x8-10
+# Goblet Squat 2x10
+# Seated Leg Curl 3x10-12
+# Leg Press Toe Press 4x10-12
+# Decline Plate Crunch 3x10-12
